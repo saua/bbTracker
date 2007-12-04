694e16ce6a429bab813b2433641406c7aeb90127
Joachim Sauer <saua@gmx.net>
2007-12-04 22:38:26 +0000

    really send data to the rest of the program! yay!

--- bbtracker/src/org/bbtracker/mobile/gps/SerialLocationProvider.java
+++ bbtracker/src/org/bbtracker/mobile/gps/SerialLocationProvider.java
@@ -1,10 +1,18 @@
 package org.bbtracker.mobile.gps;
 
+import java.util.TimerTask;
+
+import org.bbtracker.TrackPoint;
+import org.bbtracker.mobile.BBTracker;
 import org.bbtracker.mobile.Log;
 import org.bbtracker.mobile.Preferences;
 
 public class SerialLocationProvider extends LocationProvider {
 	private final Gps gps;
+
+	private TimerTask updateTask;
+
+	private int lastNmeaCount;
 
 	public SerialLocationProvider() {
 		gps = new Gps();
@@ -17,14 +25,20 @@ public class SerialLocationProvider extends LocationProvider {
 	}
 
 	public void init() throws LocationException {
-		final String url = Preferences.getInstance().getBluetoothUrl();
+		final Preferences pref = Preferences.getInstance();
+		final String url = pref.getBluetoothUrl();
 		gps.open(url);
+		setUpdateInterval(pref.getSampleInterval());
 		setState(AVAILABLE);
 	}
 
 	public void setUpdateInterval(final int updateInterval) {
 		super.setUpdateInterval(updateInterval);
-
+		if (updateTask != null) {
+			updateTask.cancel();
+		}
+		updateTask = new UpdateTask();
+		BBTracker.getTimer().schedule(updateTask, 0, updateInterval * 1000);
 	}
 
 	public int tryRecover(final int escalationLevel) {
@@ -42,5 +56,22 @@ public class SerialLocationProvider extends LocationProvider {
 				return escalationLevel * 1000; // wait a second more each level
 			}
 		}
+	}
+
+	private class UpdateTask extends TimerTask {
+
+		public void run() {
+			final int nmeaCount = gps.getNmeaCount();
+			TrackPoint p;
+			if (!gps.getFix() || nmeaCount == lastNmeaCount) {
+				p = null;
+			} else {
+				lastNmeaCount = nmeaCount;
+				p = new TrackPoint(gps.getTimestamp(), gps.getLatitude(), gps.getLongitude(), gps.getAltitude(), gps
+						.getSpeed(), gps.getHeading(), false);
+			}
+			fireLocationUpdated(null);
+		}
+
 	}
 }
