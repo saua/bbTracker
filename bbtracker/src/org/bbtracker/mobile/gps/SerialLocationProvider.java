2ba99d257cfff8f3e0a9063c9abc7303908eaa96
Joachim Sauer <saua@gmx.net>
2007-12-13 13:43:48 +0000

    handle more error cases: * lost connection to GPS puck (IOException -> no thread) * no more NMEA updates

--- bbtracker/src/org/bbtracker/mobile/gps/SerialLocationProvider.java
+++ bbtracker/src/org/bbtracker/mobile/gps/SerialLocationProvider.java
@@ -8,6 +8,9 @@ import org.bbtracker.mobile.Log;
 import org.bbtracker.mobile.Preferences;
 
 public class SerialLocationProvider extends LocationProvider {
+	// wait at most 60 seconds for new NMEA sentences, before attempting recovery
+	private static final int NO_NMEA_TIMEOUT = 60 * 1000;
+
 	private final Gps gps;
 
 	private TimerTask updateTask;
@@ -27,9 +30,14 @@ public class SerialLocationProvider extends LocationProvider {
 	public void init() throws LocationException {
 		final Preferences pref = Preferences.getInstance();
 		final String url = pref.getBluetoothUrl();
-		gps.open(url);
-		setUpdateInterval(pref.getSampleInterval());
-		setState(AVAILABLE);
+		if (url == null || url.length() == 0) {
+			Log.log(this, "No URL configured!");
+			setState(OUT_OF_SERVICE);
+		} else {
+			gps.open(url);
+			setUpdateInterval(pref.getSampleInterval());
+			setState(AVAILABLE);
+		}
 	}
 
 	public void setUpdateInterval(final int updateInterval) {
@@ -59,20 +67,30 @@ public class SerialLocationProvider extends LocationProvider {
 	}
 
 	private class UpdateTask extends TimerTask {
+		private long lastNmeaChangeTimestamp = System.currentTimeMillis();
 
 		public void run() {
 			final int nmeaCount = gps.getNmeaCount();
 			TrackPoint p;
-			Log.log(this, "Update. Fix: " + gps.getFix() + " nmeaCount: " + nmeaCount);
-			if (!gps.getFix() || nmeaCount == lastNmeaCount) {
-				p = null;
+			final boolean open = gps.isOpen();
+			if (!gps.getFix() || nmeaCount == lastNmeaCount || !open) {
+				final long lastChange = lastNmeaChangeTimestamp - System.currentTimeMillis();
+				Log.log(this, "No new GPS data. Fix: " + gps.getFix() + " nmeaCount: " + nmeaCount +
+						" lastNmeaCount: " + lastNmeaCount + " last NMEA change " + lastChange + "ms ago, GPS open? " +
+						open);
+				if (lastChange >= NO_NMEA_TIMEOUT || !open) {
+					setState(TEMPORARILY_UNAVAILABLE);
+					return;
+				} else {
+					p = null;
+				}
 			} else {
 				lastNmeaCount = nmeaCount;
+				lastNmeaChangeTimestamp = System.currentTimeMillis();
 				p = new TrackPoint(gps.getTimestamp(), gps.getLatitude(), gps.getLongitude(), gps.getAltitude(), gps
 						.getSpeed(), gps.getHeading(), false);
 			}
 			fireLocationUpdated(p);
 		}
-
 	}
 }
