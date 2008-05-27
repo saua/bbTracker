8c3acdf32965833cfcd043aca2794879466e4a6d
Joachim Sauer <saua@gmx.net>
2008-05-27 11:08:03 +0000

    documentation

--- bbtracker_common/src/org/bbtracker/CsvReader.java
+++ bbtracker_common/src/org/bbtracker/CsvReader.java
@@ -1,9 +1,29 @@
+/*
+ * Copyright 2008 Joachim Sauer
+ * 
+ * This file is part of bbTracker.
+ * 
+ * bbTracker is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * bbTracker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.bbtracker;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.util.Vector;
 
+/**
+ * The CsvReader is used to read and parse a CSV stream according to RFC 4180.
+ */
 public class CsvReader {
 	private Reader reader;
 
--- bbtracker_common/src/org/bbtracker/CsvWriter.java
+++ bbtracker_common/src/org/bbtracker/CsvWriter.java
@@ -1,5 +1,25 @@
+/*
+ * Copyright 2008 Joachim Sauer
+ * 
+ * This file is part of bbTracker.
+ * 
+ * bbTracker is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * bbTracker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.bbtracker;
 
+/**
+ * The CsvWriter is used produce a CSV stream according to RFC 4180.
+ */
 class CsvWriter {
 	private final StringBuffer buffer = new StringBuffer();
 
