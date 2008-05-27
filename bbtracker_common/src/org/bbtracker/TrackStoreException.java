0f340c88827b0be4a637c51540361a94784cfda0
Joachim Sauer <saua@gmx.net>
2008-05-27 10:50:01 +0000

    fix warning

--- bbtracker_common/src/org/bbtracker/TrackStoreException.java
+++ bbtracker_common/src/org/bbtracker/TrackStoreException.java
@@ -1,6 +1,8 @@
 package org.bbtracker;
 
 public class TrackStoreException extends Exception {
+	private static final long serialVersionUID = 1L;
+
 	public TrackStoreException(final Throwable t) {
 		super(t.toString());
 	}
